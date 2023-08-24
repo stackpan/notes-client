b0ee120af1d781ec875bdb044bb8e65a3428850e
Ivan Rizkyanto <ivanrizqy.ir@gmail.com>
2023-08-24 07:23:20 +0700

    Hide AnnounceBar and auto set backend url from env

--- components/Common/AnnounceBar/index.js
+++ components/Common/AnnounceBar/index.js
@@ -1,43 +1,13 @@
 import React, { Component } from 'react';
-import Link from 'next/link';
-import styles from './AnnounceBar.module.scss';
-import { getBaseURL, setBaseURL } from '../../../lib/utils/storage';
+import { setBaseURL } from '../../../lib/utils/storage';
 
 class AnnounceBar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      host: '/',
-    };
-  }
-
   componentDidMount() {
-    this.setState({ host: getBaseURL() });
+    setBaseURL();
   }
 
   render() {
-    const { host } = this.state;
-
-    return (
-      <div className={styles.announce_bar}>
-        <p>
-          Web Server API:
-          {' '}
-          <Link
-            href={host}
-            target="__blank"
-          >
-            {host}
-          </Link>
-          {' '}
-          |
-          {' '}
-          <button type="button" onClick={setBaseURL}>Change URL</button>
-
-        </p>
-      </div>
-    );
+    return <div />;
   }
 }
 
